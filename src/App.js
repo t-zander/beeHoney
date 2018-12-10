345e8a01a53959492257718c523a8663ec62c3af
t-Zander <alex.sergeevich.tarasenko@gmail.com>
2018-12-10 22:29:28 +0200

    basic header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
+import Layout from './containers/Layout';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h3>Hello world</h3>
-      </div>
+      <Layout/>
     );
   }
 }
