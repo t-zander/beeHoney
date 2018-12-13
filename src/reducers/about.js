a5d1b37d51c0f3da7144f62ec294f4584d188e69
t-Zander <alex.sergeevich.tarasenko@gmail.com>
2018-12-13 21:02:06 +0200

    added about us in redux

--- src/reducers/about.js
+++ src/reducers/about.js
@@ -1,1 +1,25 @@
-// about us reducer
+import * as actionTypes from '../actions/actionTypes';
+
+const initialState = {
+  about: {
+    about: '',
+    imageUrl: ''
+  },
+  loading: false
+};
+
+const aboutReducer = (state = initialState, action) => {
+  switch(action.type) {
+    case actionTypes.FETCH_ABOUT_SUCCESS:
+      console.log(action.payload);
+      return {
+        ...state,
+        about: action.payload
+      }
+
+    default:
+      return state;
+  }
+}
+
+export default aboutReducer;
