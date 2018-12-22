4d81342d07c9d2c5c2a377fdd7cd1858bde747cb
t-Zander <alex.sergeevich.tarasenko@gmail.com>
2018-12-22 19:54:46 +0200

    started working on filters

--- src/actions/products/products.js
+++ src/actions/products/products.js
@@ -13,6 +13,28 @@ export const fetchAll = () => {
   }
 }
 
+/* 
+  Можно сделать отдельные экшны для выборки по категории
+  Можно сделать один экшн selectCategory()
+  и вызывать его на 21 строке перед "fetchSuccess(response.data)"
+  Таким образом:
+  - юзер выбирает категорию
+  - вызываем "fetchByCategory"
+  - отправляем запрос к бэку
+  - приходит успешный ответ
+  - в сторе меняем выбранную категорию (нужно подумать надо ли нам это)
+  - диспатчим экшн "fetchSuccess"
+*/
+export const fetchByCategory = (categoryId) => {
+  return(dispatch) => {
+    dispatch(fetchStart());
+    return axios.get(`${config.serverUrl}products/${categoryId}`)
+      .then(response => {
+        dispatch(fetchSuccess(response.data))
+      })
+  }
+}
+
 export const fetchStart = () => {
   return {
     type: actionTypes.FETCH_PRODUCTS_START
