d60231740e29c2317850b79a70e49bd270eced58
t-Zander <alex.sergeevich.tarasenko@gmail.com>
2019-01-13 19:12:26 +0200

    styling for buttons

--- src/containers/MainPage/MainPage.js
+++ src/containers/MainPage/MainPage.js
@@ -1,8 +1,10 @@
 import React from "react";
 import "../../styles/mainPage.scss";
 import beeMovie from "../../assets/videos/beeMovie.mp4";
+import {Link} from "react-router-dom";
 
-const MainPage = () => {
+const MainPage = (props) => {
+
   return (
     <div className="mainPage">
       <video autoPlay muted loop className="mainPage__video">
@@ -17,13 +19,12 @@ const MainPage = () => {
         </p>
 
         <div className="mainPage__btnGroup">
-          <button className="mainPage__btn">Заказать</button>
-          <i className="material-icons mainPage__add">
-            add
-          </i>
-          <p className="mainPage__text">
-            Подробнее о продуктах
-          </p>
+          <button className="mainPage__btn" onClick={() => {props.history.push('/shop')}}>Заказать</button>
+          
+          <Link to="/about" className="mainPage__aboutLink" >
+            <i className="material-icons add">add</i>
+            <span className="details">Подробнее...</span>
+          </Link>
         </div>
       </div>
     </div>
